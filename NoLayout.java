fee76f2677f87b73ec152ba0867854b2c31a4447
rahul-ki <rahul.kothari.201@gmail.com>
2018-04-17 18:30:23 +0100

    Lab 11 - GUI

--- NoLayout.java
+++ NoLayout.java
@@ -27,9 +27,9 @@ public class NoLayout extends JFrame  {
       label1 = new JLabel("Username: ");
       textField1 = new JTextField(20);
       textField1.setToolTipText("Username");
-	  label2 = new JLabel("Password : ");
-	  textField2 = new JPasswordField(20);
-	  textField2.setToolTipText("Password");
+      label2 = new JLabel("Password : ");
+      textField2 = new JPasswordField(20);
+      textField2.setToolTipText("Password");
       button = new JButton("Log in");
       button.setEnabled(true);
       
@@ -42,9 +42,9 @@ public class NoLayout extends JFrame  {
 	  
       //  position the components and size them
       label1.setBounds(10, 20, 300, 50);
-	  label2.setBounds(10, 50, 300, 50);
+      label2.setBounds(10, 50, 300, 50);
       textField1.setBounds(110, 30, 200, 30);
-	  textField2.setBounds(110, 60, 200, 30);
+      textField2.setBounds(110, 60, 200, 30);
       button.setBounds(110, 90, 100, 30);
    }
 
@@ -53,7 +53,7 @@ public class NoLayout extends JFrame  {
       frame.setTitle("A login window");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setLocation(new Point (200,170));
-	  frame.setSize(new Dimension (500,200));
+      frame.setSize(new Dimension (500,200));
       frame.setVisible(true);
    }
 
